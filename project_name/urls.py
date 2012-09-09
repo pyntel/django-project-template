88e10fb740ed9bd87e103c65edfddd3d31a03f32
Pedro Kroger <kroger@pedrokroger.net>
2012-09-09 02:00:59 -0300

    decouple urls

--- project_name/urls.py
+++ project_name/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', "mainapp.views.index", name='index'),
+    url(r'', include('mainapp.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
